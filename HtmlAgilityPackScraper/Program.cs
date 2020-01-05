569163662be455aa61d5d4700c0f61b5856528a9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-01-05 15:51:05 -0500

    grabbing all the crypto data and parsing it line by line to get the info ready to be injected into database

--- HtmlAgilityPackScraper/Program.cs
+++ HtmlAgilityPackScraper/Program.cs
@@ -1,72 +1,40 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace HtmlAgilityPackScraper
 {
-  class Program
-    {
-        static void Main(string[] args)
-        {
-            WebDataScrap();
-        }
- 
-        public static void WebDataScrap()
-        {
-            try
-            {
-                const string url = "http://www.coinmarketcap.com";
-                
-                
-                // var web = new HtmlWeb();
-                // var doc = web.Load(url);
-                //
-                // //Get the content from a file
-                // //var path = "countries.html";
-                // //var doc = new HtmlDocument();
-                // //doc.Load(path);
-                //
-                // //Filter the content
-                // doc.DocumentNode.Descendants()
-                //     
-                //                 .Where(n => n.Name == "script")
-                //                 .ToList()
-                //                 .ForEach(n => n.Remove());
-                //
-                // const string classValue = "border1";
-                // var nodes = doc.DocumentNode.SelectNodes($"//*[@class='{classValue}']") ?? Enumerable.Empty<HtmlNode>();
-                //
-                // //Write the desired content to a file
-                // using (var file = new StreamWriter("test.txt"))
-                // {
-                //     foreach (var node in nodes)
-                //     {
-                //         //Get the country name
-                //         var splittedWords = Regex.Split(node.InnerText, "\n");
-                //         var words = splittedWords
-                //             .Where(x => !x.Contains("&nbsp;") && !string.IsNullOrEmpty(x.Trim()))
-                //             .ToList();
-                //
-                //         if (words.Count() != 4) continue;
-                //
-                //         var countryName = words[0].Trim();
-                //         var countryCode = words[2].Trim();
-                //         var result = $"{countryName};{countryCode}";
-                //
-                //         file.WriteLine(result);
-                //         Console.WriteLine(result);
-                //     }
-                // }
-                //
-                // Console.WriteLine("\r\nPlease press a key...");
-                // Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occured:\r\n{ex.Message}");
-            }
-        }
-    }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             GetHtmlAsync();
+             Console.WriteLine("Hit any key to exit");
+             Console.ReadLine();
+
+         }
+
+         private static async void GetHtmlAsync()
+         {
+             const string url = "http://www.coinmarketcap.com";
+            
+             HtmlWeb web = new HtmlWeb();  
+             HtmlDocument document = web.Load(url);
+
+             HtmlNode[] nodes = document.DocumentNode.SelectNodes("//tr").ToArray();  
+             foreach (HtmlNode item in nodes)
+             {
+                 var splitData = item.InnerText.Split('$');
+                 
+                 var allAlphaSymbol = Regex.Replace(splitData[0], @"[\d-]", string.Empty);
+                 
+                 Console.WriteLine(allAlphaSymbol);
+                 
+                 foreach (var word in splitData)
+                    Console.WriteLine(word);
+             }
+         }
+     }
 }
