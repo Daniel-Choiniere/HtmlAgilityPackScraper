02859aa262ccc88bb0394e325aa1d1cd9ed4d009
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-01-05 22:10:32 -0500

    succesfully parsing the html, created a Sql database with table for crypto data, connected to my Azure server, and injecting data into the new database

--- HtmlAgilityPackScraper/Program.cs
+++ HtmlAgilityPackScraper/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using Microsoft.Data.SqlClient;
 
 namespace HtmlAgilityPackScraper
 {
@@ -10,31 +11,60 @@ namespace HtmlAgilityPackScraper
      {
          static void Main(string[] args)
          {
-             GetHtmlAsync();
-             Console.WriteLine("Hit any key to exit");
-             Console.ReadLine();
-
+             GetHtml();
          }
 
-         private static async void GetHtmlAsync()
+         private static void GetHtml()
          {
              const string url = "http://www.coinmarketcap.com";
             
              HtmlWeb web = new HtmlWeb();  
              HtmlDocument document = web.Load(url);
 
-             HtmlNode[] nodes = document.DocumentNode.SelectNodes("//tr").ToArray();  
-             foreach (HtmlNode item in nodes)
-             {
-                 var splitData = item.InnerText.Split('$');
+             List<HtmlNode> nodes = document.DocumentNode.SelectNodes("//tr").ToList();
+             nodes.RemoveRange(0, 3);
+             
+                         string connection =
+                @"Server=tcp:finance-scraper.database.windows.net,1433;Initial Catalog=CoinMarketCap;Persist Security Info=False;User ID=Dan;Password=iLOVEcareerdevs1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            using (SqlConnection dbConnection = new SqlConnection(connection))
+            {
+                dbConnection.Open();
+
+                foreach (HtmlNode item in nodes)
+                {
+                    string[] splitData = item.InnerText.Split('$');
+                    // foreach (var word in splitData)
+                    //     Console.WriteLine(word);
                  
-                 var allAlphaSymbol = Regex.Replace(splitData[0], @"[\d-]", string.Empty);
+                    string currency = Regex.Replace(splitData[0], @"[\d-]", string.Empty);
+                    string marketCap = "$" + splitData[1];
+                    string price = "$" + splitData[2];
                  
-                 Console.WriteLine(allAlphaSymbol);
+                    string[] splitFurtherData = splitData[3].Split(" ");
+                    string volume = "$" + splitFurtherData[0];
                  
-                 foreach (var word in splitData)
-                    Console.WriteLine(word);
-             }
+                    string priceChange = Regex.Replace(splitFurtherData[1], "[^0-9.%]", "");
+                    
+                    SqlCommand insertCommand = new SqlCommand(
+                        "INSERT into dbo.CryptoData (DateTimeScraped, Currency, MarketCap, Price, Volume, PriceChange) VALUES (@dateTime, @currency, @marketCap, @price, @volume, @priceChange)",
+                        dbConnection);
+                    
+                    insertCommand.Parameters.AddWithValue("@dateTime", DateTime.Now);
+                    insertCommand.Parameters.AddWithValue("@currency", currency);
+                    insertCommand.Parameters.AddWithValue("@marketCap", marketCap);
+                    insertCommand.Parameters.AddWithValue("@price", price);
+                    insertCommand.Parameters.AddWithValue("@volume", volume);
+                    insertCommand.Parameters.AddWithValue("@priceChange", priceChange);
+
+                    insertCommand.ExecuteNonQuery();
+                }
+                
+                Console.WriteLine("Data Collection Successful");
+                dbConnection.Close();
+            }
+             
+             
          }
      }
 }
