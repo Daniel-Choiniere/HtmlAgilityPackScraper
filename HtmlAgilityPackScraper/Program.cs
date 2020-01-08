29aaf8f9b6752d89e088e7fb9b59cb5268f56c32
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-01-07 22:11:45 -0500

    found a better way to access table data, I will need to do a lot less maipulation of data to get hat i want

--- HtmlAgilityPackScraper/Program.cs
+++ HtmlAgilityPackScraper/Program.cs
@@ -13,7 +13,7 @@ namespace HtmlAgilityPackScraper
          {
              GetHtml();
          }
-
+         
          private static void GetHtml()
          {
              const string url = "http://www.coinmarketcap.com";
@@ -21,12 +21,53 @@ namespace HtmlAgilityPackScraper
              HtmlWeb web = new HtmlWeb();  
              HtmlDocument document = web.Load(url);
 
-             List<HtmlNode> nodes = document.DocumentNode.SelectNodes("//tr").ToList();
-             nodes.RemoveRange(0, 3);
+             List<HtmlNode> nodes = document.DocumentNode.SelectNodes("//td").ToList();
+             
+             // foreach (var node in nodes)
+             // {
+             //     Console.WriteLine(node.InnerText);
+             // }
+             var chunked = ChunkIt(nodes, 5);
 
+             for (var i = 0; i < chunked.Count; i++)
+             {
+                 Console.WriteLine(chunked[i][i].InnerText);
+             }
+             
+             // foreach (var chunk in chunked)
+             // {
+             //     foreach (var subChunk in chunk)
+             //     {
+             //         Console.WriteLine(subChunk.InnerText);
+             //     }
+             // }
+             
              InjectData(nodes);
          }
+         
+         
+         public static List<List<T>> ChunkIt<T>(List<T> nodes, int size)
+         {
+             var chunks = new List<List<T>>();
+             var count = 0;
+             var temp = new List<T>();
 
+             foreach (var element in nodes)
+             {
+                 if (count++ == size)
+                 {
+                     chunks.Add(temp);
+                     temp = new List<T>();
+                     count = 1;
+                 }
+                 temp.Add(element);
+             }
+
+             chunks.Add(temp);  
+             return chunks;
+         }
+         
+         
          private static void InjectData(List<HtmlNode> nodes)
          {
              string connection =
@@ -35,31 +76,53 @@ namespace HtmlAgilityPackScraper
             using (SqlConnection dbConnection = new SqlConnection(connection))
             {
                 dbConnection.Open();
-
-                foreach (HtmlNode item in nodes)
+                
+                foreach (HtmlNode node in nodes)
                 {
-                    string[] splitData = item.InnerText.Split('$');
-
-                    string currency = Regex.Replace(splitData[0], @"[\d-]", string.Empty);
-                    string marketCap = "$" + splitData[1];
-                    string price = "$" + splitData[2];
-                 
-                    string[] splitFurtherData = splitData[3].Split(" ");
-                    string volume = "$" + splitFurtherData[0];
-                 
-                    string priceChange = Regex.Replace(splitFurtherData[1], "[^0-9.%]", "");
+                    // Console.WriteLine(node.InnerText);
                     
-                    SqlCommand insertCommand = new SqlCommand(
-                        "INSERT into dbo.CryptoData (DateTimeScraped, Currency, MarketCap, Price, Volume, PriceChange) VALUES (@dateTime, @currency, @marketCap, @price, @volume, @priceChange)",
-                        dbConnection);
-                    insertCommand.Parameters.AddWithValue("@dateTime", DateTime.Now);
-                    insertCommand.Parameters.AddWithValue("@currency", currency);
-                    insertCommand.Parameters.AddWithValue("@marketCap", marketCap);
-                    insertCommand.Parameters.AddWithValue("@price", price);
-                    insertCommand.Parameters.AddWithValue("@volume", volume);
-                    insertCommand.Parameters.AddWithValue("@priceChange", priceChange);
-
-                    insertCommand.ExecuteNonQuery();
+                    // string[] splitData = node.InnerText.Split('$');
+                    //
+                    // string currency = Regex.Replace(splitData[0], @"[\d-]", string.Empty);
+                    // string marketCap = "$" + splitData[1];
+                    // string price = "$" + splitData[2];
+                    // string volume = "0";    
+                    //
+                    // string[] splitFurtherData = splitData[3].Split(" ");
+                    //
+                    // string[] splitVolume = splitFurtherData[0].Split(",");
+                    // foreach (string numberSet in splitVolume)
+                    // {
+                    //     if (numberSet.Length > 3)
+                    //     {
+                    //         string volumeLastNumbers = numberSet.Substring(0, 3);
+                    //         string circulatingSupplyFirstNumbers = numberSet.Substring(3);
+                    //         
+                    //         // Console.WriteLine("LastNumber: " +  volumeLastNumbers);
+                    //
+                    //         volume = splitVolume[0] + "," + splitVolume[1] + "," + splitVolume[2] + "," +
+                    //                         volumeLastNumbers;
+                    //         
+                    //         Console.WriteLine(volume);
+                    //         // Console.WriteLine("Circulating Supply: " + circulatingSupplyFirstNumbers);
+                    //     }
+                    //
+                    //     volume = "5";
+                    // }
+                    //
+                    // string priceChange = Regex.Replace(splitFurtherData[1], "[^0-9.%]", "");
+                    
+                    // SqlCommand insertCommand = new SqlCommand(
+                    //     "INSERT into dbo.CryptoData (DateTimeScraped, Currency, MarketCap, Price, Volume, PriceChange) VALUES (@dateTime, @currency, @marketCap, @price, @volume, @priceChange)",
+                    //     dbConnection);
+                    // insertCommand.Parameters.AddWithValue("@dateTime", DateTime.Now);
+                    // insertCommand.Parameters.AddWithValue("@currency", currency);
+                    // insertCommand.Parameters.AddWithValue("@marketCap", marketCap);
+                    // insertCommand.Parameters.AddWithValue("@price", price);
+                    // insertCommand.Parameters.AddWithValue("@volume", volume);
+                    // insertCommand.Parameters.AddWithValue("@priceChange", priceChange);
+                    //
+                    // insertCommand.ExecuteNonQuery();
                 }
                 
                 Console.WriteLine("Data Collection Successful");
